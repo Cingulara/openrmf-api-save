6a08a15722cd4eaa67bb112657c930b45be82654
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-07-19 11:27:31 -0400

    RBAC updates with keycloak for v0.8

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 
 using NATS.Client;
 
@@ -54,14 +49,55 @@ namespace openrmf_save_api
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "openRMF Save API", Version = "v1", 
-                    Description = "The Save API that goes with the openRMF tool",
+                c.SwaggerDoc("v1", new Info { Title = "OpenRMF Save API", Version = "v1", 
+                    Description = "The Save API that goes with the OpenRMF tool",
                     Contact = new Contact
                     {
                         Name = "Dale Bingham",
                         Email = "dale.bingham@cingulara.com",
                         Url = "https://github.com/Cingulara/openrmf-api-save"
                     } });
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.Authority = Environment.GetEnvironmentVariable("JWT-AUTHORITY");
+                o.Audience = Environment.GetEnvironmentVariable("JWT-CLIENT");
+                o.IncludeErrorDetails = true;
+                o.RequireHttpsMetadata = false;
+                o.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidIssuer = Environment.GetEnvironmentVariable("JWT-AUTHORITY"),
+                    ValidateLifetime = true
+                };
+
+                o.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+                        c.Response.StatusCode = 401;
+                        c.Response.ContentType = "text/plain";
+
+                        return c.Response.WriteAsync(c.Exception.ToString());
+                    }
+                };
+            });
+
+            // setup the RBAC for this
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Administrator", policy => policy.RequireRole("roles", "[Administrator]"));
+                options.AddPolicy("Editor", policy => policy.RequireRole("roles", "[Editor]"));
+                options.AddPolicy("Reader", policy => policy.RequireRole("roles", "[Reader]"));
+                options.AddPolicy("Assessor", policy => policy.RequireRole("roles", "[Assessor]"));
             });
 
             // ********************
@@ -108,7 +144,7 @@ namespace openrmf_save_api
             // USE CORS
             // ********************
             app.UseCors("AllowAll");
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
