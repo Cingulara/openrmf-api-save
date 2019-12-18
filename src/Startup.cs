2c76f6868060ac2ca4e888fa0672bba8388f3f20
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-12-18 06:28:29 -0500

    Naming the NATS client

--- src/Startup.cs
+++ src/Startup.cs
@@ -41,6 +41,7 @@ namespace openrmf_save_api
             Options opts = ConnectionFactory.GetDefaultOptions();
             opts.MaxReconnect = -1;
             opts.ReconnectWait = 1000;
+            opts.Name = "openrmf-api-save";
             opts.Url = Environment.GetEnvironmentVariable("NATSSERVERURL");
             opts.AsyncErrorEventHandler += (sender, events) =>
             {
